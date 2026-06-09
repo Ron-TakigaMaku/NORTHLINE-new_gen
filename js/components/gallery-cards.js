c4a64e2c1ed5ed72b5691045259c3b49aa29c325
Yevrey <boyarskiykirill1@gmail.com>
2026-06-09 16:21:00 +0300

    fix

--- js/components/gallery-cards.js
+++ js/components/gallery-cards.js
@@ -4,6 +4,13 @@ export function createGalleryCard(item) {
       <button class="gallery__btn" data-id="${item.id}">
         <img src="${item.img}" alt="NORTHLINE editorial" />
       </button>
+       <div id="gallery-modal">
+        <div class="gallery-modal__overlay"></div>
+        <div class="gallery-modal__window">
+          <button class="gallery-modal__close">✕</button>
+          <div class="gallery-modal__body"></div>
+        </div>
+    </div>
     </li>
   `
 }
